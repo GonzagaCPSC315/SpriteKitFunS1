772d110e43b42598015d33dc0c2bb9919ca149f5
Gina Sprint <sprint@gonzaga.edu>
2020-12-09 10:30:56 -0800

    add background and spike

--- SpriteKitFunS1/GameScene.swift
+++ SpriteKitFunS1/GameScene.swift
@@ -9,76 +9,42 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    // a SKScene.swift file can have an associated .sks file
+    // the .sks file is the graphical editor component to this file
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var background = SKSpriteNode()
+    var spike = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        // recall a SKView can show one or more SKScenes
+        // this method is like viewDidLoad()
+        // its called when the view "moves to" this scene
+        // put our init code in here
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // exploring our coordinate system
+        print("Frame width: \(self.frame.width) height: \(self.frame.height)")
+        print("minX: \(self.frame.minX) maxX: \(self.frame.maxX)")
+        print("minY: \(self.frame.minY) maxY: \(self.frame.maxY)")
+        print("midX: \(self.frame.midX) midY: \(self.frame.midY)")
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // add our background image as a sprite
+        background = SKSpriteNode(imageNamed: "court")
+        // set the background's size to be the same as the scene's frame
+        background.size = CGSize(width: self.frame.width, height: self.frame.height)
+        // we want our background to be "behind" all the other sprites
+        background.zPosition = -1 // default 0
+        // add the background to the scene with addChild()
+        addChild(background)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // add spike
+        spike = SKSpriteNode(imageNamed: "spike")
+        spike.size = CGSize(width: 225, height: 200)
+        addChild(spike)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
     }
     
     
--- SpriteKitFunS1/GameViewController.swift
+++ SpriteKitFunS1/GameViewController.swift
@@ -14,6 +14,12 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MARK: - Views, Scenes, Nodes
+        // A SKView show one or more SKScenes
+        // A SKScene shows one or more SKNodes
+        // Examples: SKLabelNode, SKShapeNode, SKSpriteNode
+        
+        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
